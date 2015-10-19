ea7ba036fdc07551b46aa73f8b0f4b26ab5787f9
Julien Deniau <julien.deniau@gmail.com>
2015-10-19 09:12:03 +0200

    Bump version: 0.4.2 → 0.4.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 setup(
     name='haversine',
-    version='0.4.2',
+    version='0.4.3',
     description='Calculate the distance bewteen 2 points on Earth.',
     long_description=open('README.txt').read(),
     author='Balthazar Rouberol',
