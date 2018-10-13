5b5e6ca039af19a0dd43c254e44a761b6a17ee0b
Julien Deniau <julien.deniau@gmail.com>
2018-10-13 16:16:07 +0200

    fix tests for feature

--- tests/test_haversine.py
+++ tests/test_haversine.py
@@ -1,9 +1,13 @@
 from haversine import haversine
 
-def test_haversine():
-    lyon = (45.7597, 4.8422)
-    paris = (48.8567, 2.3508)
+lyon = (45.7597, 4.8422)
+paris = (48.8567, 2.3508)
 
-    assert haversine(lyon, paris) == 392.21671780659625  # in kilometers
-    assert haversine(lyon, paris, miles=True) == 243.71209416020253  # in miles
-    assert haversine(lyon, paris, nautical_miles=True) == 211.7801622966963  # in nautical miles
+def test_kilometers():
+    assert haversine(lyon, paris) == 392.2172595594006  # in kilometers
+
+def test_miles():
+    assert haversine(lyon, paris, miles=True) == 243.7125041070121  # in miles
+
+def test_nautical_miles():
+    assert haversine(lyon, paris, nautical_miles=True) == 211.7803748731092  # in nautical miles
