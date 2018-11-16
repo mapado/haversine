b6794d1b5011df2261673ece8878a46f1fdc261d
Martin Miglio <marmig0404@gmail.com>
2018-11-16 12:52:56 -0500

    typo in tests

--- tests/test_haversine.py
+++ tests/test_haversine.py
@@ -17,12 +17,12 @@ def test_nautical_miles():
 
 
 def test_inverse_kilometers():
-    assert inverse_haversine(paris, 32, 'west') == (48.8567, 1.9134030884190545)
+    assert inverse_haversine(paris, 32, 'west') == [48.8567, 1.9134030884190545]
 
 
 def test_inverse_miles():
-    assert inverse_haversine(paris, 50, 'north', miles=True) == (49.5803579218996, 2.3508)
+    assert inverse_haversine(paris, 50, 'north', miles=True) == [49.5803579218996, 2.3508]
 
 
 def test_nautical_inverse_miles():
-    assert inverse_haversine(paris, 10 ,'south', nautical_miles=True) == (48.69014586638915, 2.3508)
+    assert inverse_haversine(paris, 10 ,'south', nautical_miles=True) == [48.69014586638915, 2.3508]
