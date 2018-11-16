641a76284320726f7c1e89652b4e5c570d83464e
Martin Miglio <marmig0404@gmail.com>
2018-11-16 12:50:07 -0500

    Added tests for inverse_haversine

--- tests/test_haversine.py
+++ tests/test_haversine.py
@@ -1,13 +1,28 @@
-from haversine import haversine
+from haversine import haversine, inverse_haversine
 
 lyon = (45.7597, 4.8422)
 paris = (48.8567, 2.3508)
 
+
 def test_kilometers():
     assert haversine(lyon, paris) == 392.2172595594006  # in kilometers
+
 
 def test_miles():
     assert haversine(lyon, paris, miles=True) == 243.7125041070121  # in miles
 
+
 def test_nautical_miles():
     assert haversine(lyon, paris, nautical_miles=True) == 211.7803748731092  # in nautical miles
+
+
+def test_inverse_kilometers():
+    assert inverse_haversine(paris, 32, 'west') == (48.8567, 1.9134030884190545)
+
+
+def test_inverse_miles():
+    assert inverse_haversine(paris, 50, 'north', miles=True) == (49.5803579218996, 2.3508)
+
+
+def test_nautical_inverse_miles():
+    assert inverse_haversine(paris, 10 ,'south', nautical_miles=True) == (48.69014586638915, 2.3508)
