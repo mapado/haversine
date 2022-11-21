3e386690431f02ea88464a1b324d83386bac72f3
Julien Deniau <julien.deniau@gmail.com>
2022-11-21 15:31:39 +0100

    haversine is stable since a long time :)

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=['haversine'],
     license='MIT',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
